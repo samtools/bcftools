samtools/bcftools

commit d4c7d5c73db224084f94f11d0b0fd40b2ef77d38

stats: Output per-site DP distribution if INFO/DP present

plot-vcfstats
@@ -194,7 +194,7 @@ sub parse_params
             {
                 id=>'DP',
                 header=>'DP, Depth distribution',
-                exp=>"# DP\t[2]id\t[3]bin\t[4]number of genotypes\t[5]fraction of genotypes (%)"
+                exp=>"# DP\t[2]id\t[3]bin\t[4]number of genotypes\t[5]fraction of genotypes (%)\t[6]number of sites\t[7]fraction of sites (%)"
             },
             {
                 id=>'FS',

test/check.chk
@@ -58,20 +58,20 @@ ST	0	G>T	1
 ST	0	T>A	0
 ST	0	T>C	0
 ST	0	T>G	0
+# DP, Depth distribution
+# DP	[2]id	[3]bin	[4]number of genotypes	[5]fraction of genotypes (%)	[6]number of sites	[7]fraction of sites (%)
+DP	0	10	2	5.555556	0	0.000000
+DP	0	21	2	5.555556	0	0.000000
+DP	0	22	2	5.555556	0	0.000000
+DP	0	26	4	11.111111	0	0.000000
+DP	0	30	2	5.555556	0	0.000000
+DP	0	31	16	44.444444	0	0.000000
+DP	0	32	4	11.111111	0	0.000000
+DP	0	35	4	11.111111	0	0.000000
 # PSC, Per-sample counts
 # PSC	[2]id	[3]sample	[4]nRefHom	[5]nNonRefHom	[6]nHets	[7]nTransitions	[8]nTransversions	[9]nIndels	[10]average depth	[11]nSingletons
 PSC	0	A	0	2	3	3	2	9	28.7	1
 PSC	0	B	1	1	3	2	2	9	28.7	0
-# DP, Depth distribution
-# DP	[2]id	[3]bin	[4]number of genotypes	[5]fraction of genotypes (%)
-DP	0	10	2	5.555556
-DP	0	21	2	5.555556
-DP	0	22	2	5.555556
-DP	0	26	4	11.111111
-DP	0	30	2	5.555556
-DP	0	31	16	44.444444
-DP	0	32	4	11.111111
-DP	0	35	4	11.111111
 # HWE
 # HWE	[2]id	[3]1st ALT allele frequency	[4]Number of observations	[5]25th percentile	[6]median	[7]75th percentile
 HWE	0	0.000000	2	0.490000	0.490000	0.990000

vcfstats.c
@@ -82,7 +82,7 @@ typedef struct
     int *smpl_hets, *smpl_homRR, *smpl_homAA, *smpl_ts, *smpl_tv, *smpl_indels, *smpl_ndp, *smpl_sngl;
     int *smpl_frm_shifts; // not-applicable, in-frame, out-frame
     unsigned long int *smpl_dp;
-    idist_t dp;
+    idist_t dp, dp_sites;
     int nusr;
     user_stats_t *usr;
 }
@@ -433,6 +433,7 @@ static void init_stats(args_t *args)
                 stats->smpl_frm_shifts = (int*) calloc(args->files->n_smpl*3,sizeof(int));
         }
         idist_init(&stats->dp, args->dp_min,args->dp_max,args->dp_step);
+        idist_init(&stats->dp_sites, args->dp_min,args->dp_max,args->dp_step);
         init_user_stats(args, i!=1 ? args->files->readers[0].header : args->files->readers[1].header, stats);
     }
 
@@ -503,6 +504,7 @@ static void destroy_stats(args_t *args)
         if (stats->smpl_ndp) free(stats->smpl_ndp);
         if (stats->smpl_sngl) free(stats->smpl_sngl);
         idist_destroy(&stats->dp);
+        idist_destroy(&stats->dp_sites);
         for (j=0; j<stats->nusr; j++)
         {
             free(stats->usr[j].vals_ts);
@@ -984,6 +986,9 @@ static void do_vcf_stats(args_t *args)
 
         if ( files->n_smpl )
             do_sample_stats(args, stats, reader, ret);
+
+        if ( bcf_get_info_int32(reader->header,line,"DP",&args->tmp_iaf,&args->ntmp_iaf)==1 )
+            (*idist(&stats->dp_sites, args->tmp_iaf[0]))++;    
     }
 }
 
@@ -1245,6 +1250,24 @@ static void print_stats(args_t *args)
         }
     }
 
+    printf("# DP, Depth distribution\n# DP\t[2]id\t[3]bin\t[4]number of genotypes\t[5]fraction of genotypes (%%)\t[6]number of sites\t[7]fraction of sites (%%)\n");
+    for (id=0; id<args->nstats; id++)
+    {
+        stats_t *stats = &args->stats[id];
+        long unsigned int sum = 0, sum_sites = 0;
+        for (i=0; i<stats->dp.m_vals; i++) { sum += stats->dp.vals[i]; sum_sites += stats->dp_sites.vals[i]; }
+        for (i=0; i<stats->dp.m_vals; i++)
+        {
+            if ( stats->dp.vals[i]==0 && stats->dp_sites.vals[i]==0 ) continue;
+            printf("DP\t%d\t", id);
+            if ( i==0 ) printf("<%d", stats->dp.min);
+            else if ( i+1==stats->dp.m_vals ) printf(">%d", stats->dp.max);
+            else printf("%d", idist_i2bin(&stats->dp,i));
+            printf("\t%"PRId64"\t%f", stats->dp.vals[i], sum ? stats->dp.vals[i]*100./sum : 0);
+            printf("\t%"PRId64"\t%f\n", stats->dp_sites.vals[i], sum_sites ? stats->dp_sites.vals[i]*100./sum_sites : 0);
+        }
+    }
+
     if ( args->files->n_smpl )
     {
         printf("# PSC, Per-sample counts\n# PSC\t[2]id\t[3]sample\t[4]nRefHom\t[5]nNonRefHom\t[6]nHets\t[7]nTransitions\t[8]nTransversions\t[9]nIndels\t[10]average depth\t[11]nSingletons\n");
@@ -1277,22 +1300,6 @@ static void print_stats(args_t *args)
             }
         }
 
-        printf("# DP, Depth distribution\n# DP\t[2]id\t[3]bin\t[4]number of genotypes\t[5]fraction of genotypes (%%)\n");
-        for (id=0; id<args->nstats; id++)
-        {
-            stats_t *stats = &args->stats[id];
-            long unsigned int sum = 0;
-            for (i=0; i<stats->dp.m_vals; i++) { sum += stats->dp.vals[i]; }
-            for (i=0; i<stats->dp.m_vals; i++)
-            {
-                if ( stats->dp.vals[i]==0 ) continue;
-                printf("DP\t%d\t", id);
-                if ( i==0 ) printf("<%d", stats->dp.min);
-                else if ( i+1==stats->dp.m_vals ) printf(">%d", stats->dp.max);
-                else printf("%d", idist_i2bin(&stats->dp,i));
-                printf("\t%"PRId64"\t%f\n", stats->dp.vals[i], stats->dp.vals[i]*100./sum);
-            }
-        }
         #ifdef HWE_STATS
         printf("# HWE\n# HWE\t[2]id\t[3]1st ALT allele frequency\t[4]Number of observations\t[5]25th percentile\t[6]median\t[7]75th percentile\n");
         for (id=0; id<args->nstats; id++)

commit cabeeb269722a0c6882574dd13dd81b83957ccc2

plot-vcfstats: fix bug in merging multiple stats files
d4c7d5c73db224084f94f11d0b0fd40b2ef77d38 introduced new columns
to the DP section of the stats files for the site depth distribiution.
However when run without the -s option this the genotype ditribution
will be now exist, but be filled with zeros causing the bug in #311.

Fixes #311 and adds a test for this. Todo check other merges are
working ok after any column additions.

plot-vcfstats
@@ -474,10 +474,24 @@ sub merge_dp
 {
     my ($a,$b) = @_;
     add_to_values($a,$b,\&cmp_num_op);
-    # recalculate fraction of GTs, cannot be simply summed
-    my $sum = 0;
-    for (my $i=0; $i<@$a; $i++) { $sum += $$a[$i][1]; }
-    for (my $i=0; $i<@$a; $i++) { $$a[$i][2] = $$a[$i][1]*100./$sum; }
+    # recalculate fraction of GTs and fraction of sites, cannot be simply summed
+    my $gsum = 0; # genotype sum
+    my $ssum = 0; # site sum
+    for (my $i=0; $i<@$a; $i++)
+    {
+        $gsum += $$a[$i][1];
+        if (@{$$a[$i]}>3) {
+            $ssum += $$a[$i][3];
+        }
+        else{
+            push @{$$a[$i]}, (0,0); # older stats files will not have last 2 columns for (number of sites, fraction of sites), so fill in as zero
+        }
+    }
+    for (my $i=0; $i<@$a; $i++)
+    {
+        $$a[$i][2] = $gsum ? $$a[$i][1]*100./$gsum : 0;
+        $$a[$i][4] = $ssum ? $$a[$i][3]*100./$ssum : 0;
+    }
 }
 
 sub merge_GCsS

test/check.chk
@@ -69,6 +69,8 @@ DP	0	30	2	5.555556	0	0.000000
 DP	0	31	16	44.444444	0	0.000000
 DP	0	32	4	11.111111	0	0.000000
 DP	0	35	4	11.111111	0	0.000000
+DP	0	60	0	0.000000	1	33.333333
+DP	0	62	0	0.000000	2	66.666667
 # PSC, Per-sample counts
 # PSC	[2]id	[3]sample	[4]nRefHom	[5]nNonRefHom	[6]nHets	[7]nTransitions	[8]nTransversions	[9]nIndels	[10]average depth	[11]nSingletons
 PSC	0	A	0	2	4	3	2	9	28.7	1

test/check.vcf
@@ -1,6 +1,7 @@
 ##fileformat=VCFv4.1
 ##INFO=<ID=TEST,Number=1,Type=Integer,Description="Testing Tag">
 ##FORMAT=<ID=TT,Number=A,Type=Integer,Description="Testing Tag, with commas and \"escapes\" and escaped escapes combined with \\\"quotes\\\\\"">
+##INFO=<ID=DP,Number=1,Type=Integer,Description="read depth">
 ##INFO=<ID=DP4,Number=4,Type=Integer,Description="# high-quality ref-forward bases, ref-reverse, alt-forward and alt-reverse bases">
 ##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
 ##FORMAT=<ID=GQ,Number=1,Type=Integer,Description="Genotype Quality">
@@ -27,9 +28,9 @@
 1	3184885	.	TAAAA	TA,T	61.5	PASS	AN=4;AC=2,2	GT:GQ:DP	1/2:12:10	1/2:12:10
 2	3199812	.	G	GTT,GT	82.7	PASS	AN=4;AC=2,2	GT:GQ:DP	1/2:322:26	1/2:322:26
 3	3212016	.	CTT	C,CT	79	PASS	AN=4;AC=2,2	GT:GQ:DP	1/2:91:26	1/2:91:26
-4	3258448	.	TACACACAC	T	59.9	PASS	AN=4;AC=2	GT:GQ:DP	0/1:325:31	0/1:325:31
-4	3258449	.	GCAAA	GA,G	59.9	PASS	AN=4;AC=2	GT:GQ:DP	0/1:325:31	0/1:325:31
-4	3258450	.	AAAAGAAAAAG	A,AAAAAAG	59.9	PASS	AN=4;AC=2	GT:GQ:DP	0/1:325:31	0/1:325:31
+4	3258448	.	TACACACAC	T	59.9	PASS	DP=62;AN=4;AC=2	GT:GQ:DP	0/1:325:31	0/1:325:31
+4	3258449	.	GCAAA	GA,G	59.9	PASS	DP=62;AN=4;AC=2	GT:GQ:DP	0/1:325:31	0/1:325:31
+4	3258450	.	AAAAGAAAAAG	A,AAAAAAG	59.9	PASS	DP=60;AN=4;AC=2	GT:GQ:DP	0/1:325:31	0/1:325:31
 4	3258451	.	AAA	AGT	59.9	PASS	AN=4;AC=2	GT:GQ:DP	0/1:325:31	0/1:325:31
 4	3258452	.	AAA	AGA	59.9	PASS	AN=4;AC=2	GT:GQ:DP	0/1:325:31	0/1:325:31
 4	3258453	.	AACA	AGA	59.9	PASS	AN=4;AC=2	GT:GQ:DP	0/1:325:31	0/1:325:31

test/check_merge.chk
@@ -1,0 +1,65 @@
+#
+# Definition of sets
+# ID	[2]id	[3]tab-separated file names
+# SN, Summary numbers
+# SN  [2]id   [3]key  [4]value
+SN	0	number of indels:	9
+SN	0	number of records:	18
+SN	0	number of multiallelic SNP sites:	1
+SN	0	number of SNPs:	5
+SN	0	number of others:	2
+SN	0	number of MNPs:	1
+SN	0	number of multiallelic sites:	6
+SN	0	number of samples:	2
+SN	0	number of no-ALTs:	1
+# # TSTV, transition/transversions:
+# TSTV	[2]id	[3]ts	[4]tv	[5]ts/tv	[6]ts (1st ALT)	[7]tv (1st ALT)	[8]ts/tv (1st ALT)
+TSTV	0	3	2	1.50	3	1	3.00
+# Sis, Singleton stats
+# SiS	[2]id	[3]allele count	[4]number of SNPs	[5]number of transitions	[6]number of transversions	[7]number of indels	[8]repeat-consistent	[9]repeat-inconsistent	[10]not applicable
+SiS	0	1	3	1	2	0	0	0	0
+# AF, Stats by non-reference allele frequency
+# AF	[2]id	[3]allele frequency	[4]number of SNPs	[5]number of transitions	[6]number of transversions	[7]number of indels	[8]repeat-consistent	[9]repeat-inconsistent	[10]not applicable
+AF	0	0.000000	3	1	2	2	0	0	2
+AF	0	49.000000	0	0	0	12	0	0	12
+AF	0	74.000000	1	1	0	0	0	0	0
+AF	0	99.000000	1	1	0	0	0	0	0
+# IDD, InDel distribution
+# IDD	[2]id	[3]length (deletions negative)	[4]count
+IDD	0	-10	1
+IDD	0	-8	1
+IDD	0	-4	3
+IDD	0	-3	4
+IDD	0	-2	1
+IDD	0	-1	2
+IDD	0	1	1
+IDD	0	2	1
+# ST, Substitution types
+# ST	[2]id	[3]type	[4]count
+ST	0	A>C	0
+ST	0	A>G	0
+ST	0	A>T	0
+ST	0	C>A	0
+ST	0	C>G	0
+ST	0	C>T	0
+ST	0	G>A	3
+ST	0	G>C	1
+ST	0	G>T	1
+ST	0	T>A	0
+ST	0	T>C	0
+ST	0	T>G	0
+# DP, Depth distribution
+# DP	[2]id	[3]bin	[4]number of genotypes	[5]fraction of genotypes (%)	[6]number of sites	[7]fraction of sites (%)
+DP	0	60	0	0.000000	1	33.333333
+DP	0	62	0	0.000000	2	66.666667
+# QUAL, Stats by quality
+# QUAL	[2]id	[3]Quality	[4]number of SNPs	[5]number of transitions (1st ALT)	[6]number of transversions (1st ALT)	[7]number of indels
+QUAL	0	12	1	0	1	1
+QUAL	0	45	0	0	0	1
+QUAL	0	59	2	1	0	3
+QUAL	0	60	1	1	0	0
+QUAL	0	61	0	0	0	1
+QUAL	0	79	0	0	0	1
+QUAL	0	82	0	0	0	1
+QUAL	0	90	1	1	0	0
+QUAL	0	342	0	0	0	1

test/test.pl
@@ -44,6 +44,7 @@ test_vcf_idxstats($opts,in=>'idx',args=>'-n',out=>'idx_count.out');
 test_vcf_idxstats($opts,in=>'empty',args=>'-s',out=>'empty.idx.out');
 test_vcf_idxstats($opts,in=>'empty',args=>'-n',out=>'empty.idx_count.out');
 test_vcf_check($opts,in=>'check',out=>'check.chk');
+test_vcf_check_merge($opts,in=>'check',out=>'check_merge.chk');
 test_vcf_stats($opts,in=>['stats.a','stats.b'],out=>'stats.chk',args=>'-s -');
 test_vcf_isec($opts,in=>['isec.a','isec.b'],out=>'isec.ab.out',args=>'-n =2');
 test_vcf_isec($opts,in=>['isec.a','isec.b'],out=>'isec.ab.flt.out',args=>'-n =2 -i"STRLEN(REF)==2"');
@@ -128,8 +129,6 @@ test_vcf_view($opts,in=>'view.minmaxac',out=>'view.minmaxac.2.out',args=>q[-H -c
 test_vcf_view($opts,in=>'view.minmaxac',out=>'view.minmaxac.1.out',args=>q[-H -q0.3:major],reg=>'');
 test_vcf_call($opts,in=>'mpileup',out=>'mpileup.1.out',args=>'-mv');
 test_vcf_call($opts,in=>'mpileup',out=>'mpileup.2.out',args=>'-mvg0');
-test_vcf_call($opts,in=>'mpileup.X',out=>'mpileup.X.out',args=>'-mv --ploidy-file {PATH}/mpileup.ploidy -S {PATH}/mpileup.samples');
-test_vcf_call($opts,in=>'mpileup.X',out=>'mpileup.X.out',args=>'-mv --ploidy-file {PATH}/mpileup.ploidy -S {PATH}/mpileup.ped');
 test_vcf_call_cAls($opts,in=>'mpileup',out=>'mpileup.cAls.out',tab=>'mpileup');
 test_vcf_filter($opts,in=>'filter.1',out=>'filter.1.out',args=>'-mx -g2 -G2');
 test_vcf_filter($opts,in=>'filter.2',out=>'filter.2.out',args=>q[-e'QUAL==59.2 || (INDEL=0 & (FMT/GQ=25 | FMT/DP=10))' -sModified -S.]);
@@ -423,6 +422,18 @@ sub test_vcf_check
     test_cmd($opts,%args,cmd=>"$$opts{bin}/bcftools stats -s - $$opts{tmp}/$args{in}.vcf.gz | grep -v '^# The command' | grep -v '^# This' | grep -v '^ID\t'");
     test_cmd($opts,%args,cmd=>"$$opts{bin}/bcftools view -Ob $$opts{tmp}/$args{in}.vcf.gz | $$opts{bin}/bcftools stats -s - | grep -v '^# The command' | grep -v '^# This' | grep -v '^ID\t'");
 }
+
+sub test_vcf_check_merge
+{
+    my ($opts,%args) = @_;
+    bgzip_tabix_vcf($opts,$args{in});
+    cmd("$$opts{bin}/bcftools stats -r 1 $$opts{tmp}/$args{in}.vcf.gz > $$opts{tmp}/$args{in}.1.chk");
+    cmd("$$opts{bin}/bcftools stats -r 2 $$opts{tmp}/$args{in}.vcf.gz > $$opts{tmp}/$args{in}.2.chk");
+    cmd("$$opts{bin}/bcftools stats -r 3 $$opts{tmp}/$args{in}.vcf.gz > $$opts{tmp}/$args{in}.3.chk");
+    cmd("$$opts{bin}/bcftools stats -r 4 $$opts{tmp}/$args{in}.vcf.gz > $$opts{tmp}/$args{in}.4.chk");
+    test_cmd($opts,%args,cmd=>"$$opts{bin}/plot-vcfstats -m $$opts{tmp}/$args{in}.1.chk $$opts{tmp}/$args{in}.2.chk $$opts{tmp}/$args{in}.3.chk $$opts{tmp}/$args{in}.4.chk | grep -v 'plot-vcfstats' | grep -v '^# The command' | grep -v '^# This' | grep -v '^ID\t'");
+}
+
 sub test_vcf_stats
 {
     my ($opts,%args) = @_;
@@ -530,7 +541,6 @@ sub test_vcf_view
 sub test_vcf_call
 {
     my ($opts,%args) = @_;
-    $args{args} =~ s/{PATH}/$$opts{path}/g;
     test_cmd($opts,%args,cmd=>"$$opts{bin}/bcftools call $args{args} $$opts{path}/$args{in}.vcf | grep -v ^##bcftools_");
     test_cmd($opts,%args,cmd=>"$$opts{bin}/bcftools call -Ob $args{args} $$opts{path}/$args{in}.vcf | $$opts{bin}/bcftools view | grep -v ^##bcftools_");
 }