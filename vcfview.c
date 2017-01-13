1618245ca7336c4c5be7dd34fb87091975b61689
Shane McCarthy <sm15@sanger.ac.uk>
2017-01-13 16:37:28 +0000

    view: update trimming error check after samtools/htslib#453

--- vcfview.c
+++ vcfview.c
@@ -444,7 +444,7 @@ int subset_vcf(args_t *args, bcf1_t *line)
     if (args->trim_alts)
     {
         int ret = bcf_trim_alleles(args->hsub ? args->hsub : args->hdr, line);
-        if ( ret==-1 ) error("Error: some GT index is out of bounds at %s:%d\n", bcf_seqname(args->hsub ? args->hsub : args->hdr, line), line->pos+1);
+        if ( ret<0 ) error("Error: Could not trim alleles at %s:%d\n", bcf_seqname(args->hsub ? args->hsub : args->hdr, line), line->pos+1);
     }
     if (args->phased) {
         int phased = bcf_all_phased(args->hdr, line);
