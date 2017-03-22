01068b0b0c3af57afc8b78821b77b0cc08c0000c
Shane McCarthy <sm15@sanger.ac.uk>
2017-03-22 13:58:43 +0000

    update Makefile and configure for bcftools rather than samtools
    remove config.h.in

--- plugins/fill-from-fasta.mk
+++ plugins/fill-from-fasta.mk
@@ -1,2 +1,2 @@
 plugins/fill-from-fasta.so: plugins/fill-from-fasta.c version.h version.c filter.h filter.c
-	$(CC) $(PLUGIN_FLAGS) $(CFLAGS) $(EXTRA_CPPFLAGS) $(CPPFLAGS) $(LDFLAGS) -o $@ filter.c version.c $< $(LIBS)
+	$(CC) $(PLUGIN_FLAGS) $(CFLAGS) $(ALL_CPPFLAGS) $(EXTRA_CPPFLAGS) $(LDFLAGS) -o $@ filter.c version.c $< $(LIBS)
