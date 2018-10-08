samtools/bcftools

commit 781c1f385006cebe777c9d873022d6cd4963d0b0

Let `norm -d` remove duplicates also on output
and update the man page to reflect the real parameters.

Resolves #674

vcfnorm.c
@@ -1514,6 +1514,7 @@ static void flush_buffer(args_t *args, htsFile *file, int n)
 {
     bcf1_t *line;
     int i, k;
+    int prev_rid = -1, prev_pos = -1, prev_type = 0;
     for (i=0; i<n; i++)
     {
         k = rbuf_shift(&args->rbuf);
@@ -1533,6 +1534,23 @@ static void flush_buffer(args_t *args, htsFile *file, int n)
                 mrows_schedule(args, &args->lines[k]);
                 continue;
             }
+        }
+        else if ( args->rmdup )
+        {
+            int line_type = bcf_get_variant_types(args->lines[k]);
+            if ( prev_rid>=0 && prev_rid==args->lines[k]->rid && prev_pos==args->lines[k]->pos )
+            {
+                if ( (args->rmdup>>1)&COLLAPSE_ANY ) continue;
+                if ( (args->rmdup>>1)&COLLAPSE_SNPS && line_type&(VCF_SNP|VCF_MNP) && prev_type&(VCF_SNP|VCF_MNP) ) continue;
+                if ( (args->rmdup>>1)&COLLAPSE_INDELS && line_type&(VCF_INDEL) && prev_type&(VCF_INDEL) ) continue;
+            }
+            else
+            {
+                prev_rid  = args->lines[k]->rid;
+                prev_pos  = args->lines[k]->pos;
+                prev_type = 0;
+            }
+            prev_type |= line_type;
         }
         bcf_write1(file, args->hdr, args->lines[k]);
     }

doc/bcftools.1
@@ -2,12 +2,12 @@
 .\"     Title: bcftools
 .\"    Author: [see the "AUTHORS" section]
 .\" Generator: DocBook XSL Stylesheets v1.76.1 <http://docbook.sf.net/>
-.\"      Date: 2017-07-14 14:40 BST
+.\"      Date: 2017-09-05 15:55 BST
 .\"    Manual: \ \&
 .\"    Source: \ \&
 .\"  Language: English
 .\"
-.TH "BCFTOOLS" "1" "2017\-07\-14 14:40 BST" "\ \&" "\ \&"
+.TH "BCFTOOLS" "1" "2017\-09\-05 15:55 BST" "\ \&" "\ \&"
 .\" -----------------------------------------------------------------
 .\" * Define some portability stuff
 .\" -----------------------------------------------------------------
@@ -41,7 +41,7 @@ Most commands accept VCF, bgzipped VCF and BCF with filetype detected automatica
 BCFtools is designed to work on a stream\&. It regards an input file "\-" as the standard input (stdin) and outputs to the standard output (stdout)\&. Several commands can thus be combined with Unix pipes\&.
 .SS "VERSION"
 .sp
-This manual page was last updated \fB2017\-07\-14 14:40 BST\fR and refers to bcftools git version \fB1\&.5\-10\-gf0ec4a3+\fR\&.
+This manual page was last updated \fB2017\-09\-05 15:55 BST\fR and refers to bcftools git version \fB1\&.5\-31\-g8198b0f+\fR\&.
 .SS "BCF1"
 .sp
 The BCF1 format output by versions of samtools <= 0\&.1\&.19 is \fBnot\fR compatible with this version of bcftools\&. To read BCF1 files one can use the view command from old versions of bcftools packaged with samtools versions <= 0\&.1\&.19 to convert to VCF, which can then be read by this version of bcftools\&.
@@ -474,7 +474,7 @@ cannot be used in combination with
 .PP
 \fB\-s, \-\-samples\fR [^]\fILIST\fR
 .RS 4
-Comma\-separated list of samples to include or exclude if prefixed with "^"\&. Note that in general tags such as INFO/AC, INFO/AN, etc are not updated to correspond to the subset samples\&.
+Comma\-separated list of samples to include or exclude if prefixed with "^"\&. The sample order is updated to reflect that given on the command line\&. Note that in general tags such as INFO/AC, INFO/AN, etc are not updated to correspond to the subset samples\&.
 \fBbcftools view\fR
 is the exception where some tags will be updated (unless the
 \fB\-I, \-\-no\-update\fR
@@ -499,7 +499,7 @@ into that command\&. For example:
 .RS 4
 File of sample names to include or exclude if prefixed with "^"\&. One sample per line\&. See also the note above for the
 \fB\-s, \-\-samples\fR
-option\&. The command
+option\&. The sample order is updated to reflect that given in the input file\&. The command
 \fBbcftools call\fR
 accepts an optional second column indicating ploidy (0, 1 or 2) or sex (as defined by
 \fB\-\-ploidy\fR, for example "F" or "M"), and can parse also PED files\&. If the second column is not present, the sex "F" is assumed\&. With
@@ -2972,13 +2972,12 @@ and
 can swap alleles and will update genotypes (GT) and AC counts, but will not attempt to fix PL or other fields\&.
 .RE
 .PP
-\fB\-d, \-\-rm\-dup\fR \fIsnps\fR|\fIindels\fR|\fIboth\fR|\fIall\fR|\fInone\fR
+\fB\-d, \-\-rm\-dup\fR \fIsnps\fR|\fIindels\fR|\fIboth\fR|\fIany\fR
 .RS 4
 If a record is present multiple times, output only the first instance, see
 \fB\-\-collapse\fR
 in
-\fBCommon Options\fR\&. Requires
-\fB\-a, \-\-allow\-overlaps\fR\&.
+\fBCommon Options\fR\&.
 .RE
 .PP
 \fB\-D, \-\-remove\-duplicates\fR

doc/bcftools.html
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><title>bcftools</title><link rel="stylesheet" type="text/css" href="docbook-xsl.css" /><meta name="generator" content="DocBook XSL Stylesheets V1.76.1" /></head><body><div xml:lang="en" class="refentry" title="bcftools" lang="en"><a id="idp207728"></a><div class="titlepage"></div><div class="refnamediv"><h2>Name</h2><p>bcftools — utilities for variant calling and manipulating VCFs and BCFs.</p></div><div class="refsynopsisdiv" title="Synopsis"><a id="_synopsis"></a><h2>Synopsis</h2><p><span class="strong"><strong>bcftools</strong></span> [--version|--version-only] [--help] [<span class="emphasis"><em>COMMAND</em></span>] [<span class="emphasis"><em>OPTIONS</em></span>]</p></div><div class="refsect1" title="DESCRIPTION"><a id="_description"></a><h2>DESCRIPTION</h2><p>BCFtools  is  a set of utilities that manipulate variant calls in the Variant
+<html xmlns="http://www.w3.org/1999/xhtml"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8" /><title>bcftools</title><link rel="stylesheet" type="text/css" href="docbook-xsl.css" /><meta name="generator" content="DocBook XSL Stylesheets V1.76.1" /></head><body><div xml:lang="en" class="refentry" title="bcftools" lang="en"><a id="idp145200"></a><div class="titlepage"></div><div class="refnamediv"><h2>Name</h2><p>bcftools — utilities for variant calling and manipulating VCFs and BCFs.</p></div><div class="refsynopsisdiv" title="Synopsis"><a id="_synopsis"></a><h2>Synopsis</h2><p><span class="strong"><strong>bcftools</strong></span> [--version|--version-only] [--help] [<span class="emphasis"><em>COMMAND</em></span>] [<span class="emphasis"><em>OPTIONS</em></span>]</p></div><div class="refsect1" title="DESCRIPTION"><a id="_description"></a><h2>DESCRIPTION</h2><p>BCFtools  is  a set of utilities that manipulate variant calls in the Variant
 Call Format (VCF) and its binary counterpart BCF. All commands work
 transparently with both VCFs and BCFs, both uncompressed and BGZF-compressed.</p><p>Most commands accept VCF, bgzipped VCF and BCF with filetype detected
 automatically even when streaming from a pipe. Indexed VCF and BCF
@@ -8,7 +8,7 @@ will work in all situations. Un-indexed VCF and BCF and streams will
 work in most, but not all situations. In general, whenever multiple VCFs are
 read simultaneously, they must be indexed and therefore also compressed.</p><p>BCFtools is designed to work on a stream. It regards an input file "-" as the
 standard input (stdin) and outputs to the standard output (stdout). Several
-commands can thus be  combined  with  Unix pipes.</p><div class="refsect2" title="VERSION"><a id="_version"></a><h3>VERSION</h3><p>This manual page was last updated <span class="strong"><strong>2017-07-14 14:40 BST</strong></span> and refers to bcftools git version <span class="strong"><strong>1.5-10-gf0ec4a3+</strong></span>.</p></div><div class="refsect2" title="BCF1"><a id="_bcf1"></a><h3>BCF1</h3><p>The BCF1 format output by versions of samtools &lt;= 0.1.19 is <span class="strong"><strong>not</strong></span>
+commands can thus be  combined  with  Unix pipes.</p><div class="refsect2" title="VERSION"><a id="_version"></a><h3>VERSION</h3><p>This manual page was last updated <span class="strong"><strong>2017-09-05 15:55 BST</strong></span> and refers to bcftools git version <span class="strong"><strong>1.5-31-g8198b0f+</strong></span>.</p></div><div class="refsect2" title="BCF1"><a id="_bcf1"></a><h3>BCF1</h3><p>The BCF1 format output by versions of samtools &lt;= 0.1.19 is <span class="strong"><strong>not</strong></span>
 compatible with this version of bcftools. To read BCF1 files one can use
 the view command from old versions of bcftools packaged with samtools
 versions &lt;= 0.1.19 to convert to VCF, which can then be read by
@@ -169,6 +169,7 @@ specific commands to see if they apply.</p><div class="variablelist"><dl><dt><sp
 </span></dt><dd>
     Comma-separated list of samples to include or exclude if prefixed
     with "^".
+    The sample order is updated to reflect that given on the command line.
     Note that in general tags such as INFO/AC, INFO/AN, etc are not updated
     to correspond to the subset samples. <span class="strong"><strong><a class="link" href="#view" title="bcftools view [OPTIONS] file.vcf.gz [REGION […]]">bcftools view</a></strong></span> is the
     exception where some tags will be updated (unless the <span class="strong"><strong>-I, --no-update</strong></span>
@@ -181,6 +182,7 @@ specific commands to see if they apply.</p><div class="variablelist"><dl><dt><sp
     File of sample names to include or exclude if prefixed with "^".
     One sample per line. See also the note above for the <span class="strong"><strong>-s, --samples</strong></span>
     option.
+    The sample order is updated to reflect that given in the input file.
     The command <span class="strong"><strong><a class="link" href="#call" title="bcftools call [OPTIONS] FILE">bcftools call</a></strong></span> accepts an optional second
     column indicating ploidy (0, 1 or 2) or sex (as defined by
     <span class="strong"><strong><a class="link" href="#ploidy">--ploidy</a></strong></span>, for example "F" or "M"), and can parse also PED
@@ -1771,11 +1773,10 @@ the <span class="strong"><strong><a class="link" href="#fasta_ref">--fasta-ref</
     can swap alleles and will update genotypes (GT) and AC counts,
     but will not attempt to fix PL or other fields.
 </dd><dt><span class="term">
-<span class="strong"><strong>-d, --rm-dup</strong></span> <span class="emphasis"><em>snps</em></span>|<span class="emphasis"><em>indels</em></span>|<span class="emphasis"><em>both</em></span>|<span class="emphasis"><em>all</em></span>|<span class="emphasis"><em>none</em></span>
+<span class="strong"><strong>-d, --rm-dup</strong></span> <span class="emphasis"><em>snps</em></span>|<span class="emphasis"><em>indels</em></span>|<span class="emphasis"><em>both</em></span>|<span class="emphasis"><em>any</em></span>
 </span></dt><dd>
     If a record is present multiple times, output only the first instance,
     see <span class="strong"><strong>--collapse</strong></span> in <span class="strong"><strong><a class="link" href="#common_options" title="Common Options">Common Options</a></strong></span>.
-    Requires <span class="strong"><strong>-a, --allow-overlaps</strong></span>.
 </dd><dt><span class="term">
 <span class="strong"><strong>-D, --remove-duplicates</strong></span>
 </span></dt><dd>

commit 1ce0f47e8d6ae35502a923baa0bd86c698b6e101

Fix a bug in duplicates removal.
This bug affects `-d none` and was introduced by 781c1f38: also unique
allele-positions could be removed.

Fixes #899

vcfnorm.c
@@ -61,6 +61,13 @@ typedef struct
     char *ref, *alt;
     void *hash;
 }
+cmpals1_t;
+
+typedef struct
+{
+    cmpals1_t *cmpals;
+    int ncmpals, mcmpals;
+}
 cmpals_t;
 
 typedef struct
@@ -83,8 +90,7 @@ typedef struct
     int aln_win;            // the realignment window size (maximum repeat size)
     bcf_srs_t *files;       // using the synced reader only for -r option
     bcf_hdr_t *hdr;
-    cmpals_t *cmpals;
-    int ncmpals, mcmpals;
+    cmpals_t cmpals_in, cmpals_out;
     faidx_t *fai;
     struct { int tot, set, swap; } nref;
     char **argv, *output_fname, *ref_fname, *vcf_fname, *region, *targets;
@@ -1535,11 +1541,11 @@ static bcf1_t *mrows_flush(args_t *args)
     }
     return NULL;
 }
-static void cmpals_add(args_t *args, bcf1_t *rec)
+static void cmpals_add(cmpals_t *ca, bcf1_t *rec)
 {
-    args->ncmpals++;
-    hts_expand0(cmpals_t, args->ncmpals, args->mcmpals, args->cmpals);
-    cmpals_t *cmpals = args->cmpals + args->ncmpals - 1;
+    ca->ncmpals++;
+    hts_expand0(cmpals1_t, ca->ncmpals, ca->mcmpals, ca->cmpals);
+    cmpals1_t *cmpals = ca->cmpals + ca->ncmpals - 1;
     free(cmpals->ref);
     cmpals->ref = strdup(rec->d.allele[0]);
     cmpals->n   = rec->n_allele;
@@ -1557,12 +1563,12 @@ static void cmpals_add(args_t *args, bcf1_t *rec)
             khash_str2int_inc(cmpals->hash, strdup(rec->d.allele[i]));
     }
 }
-static int cmpals_match(args_t *args, bcf1_t *rec)
+static int cmpals_match(cmpals_t *ca, bcf1_t *rec)
 {
     int i, j;
-    for (i=0; i<args->ncmpals; i++)
+    for (i=0; i<ca->ncmpals; i++)
     {
-        cmpals_t *cmpals = args->cmpals + i;
+        cmpals1_t *cmpals = ca->cmpals + i;
         if ( rec->n_allele != cmpals->n ) continue;
 
         // NB. assuming both are normalized
@@ -1581,21 +1587,20 @@ static int cmpals_match(args_t *args, bcf1_t *rec)
         if ( j<rec->n_allele ) continue;
         return 1;
     }
-    cmpals_add(args, rec);
     return 0;
 }
-static void cmpals_reset(args_t *args) { args->ncmpals = 0; }
-static void cmpals_destroy(args_t *args)
+static void cmpals_reset(cmpals_t *ca) { ca->ncmpals = 0; }
+static void cmpals_destroy(cmpals_t *ca)
 {
     int i;
-    for (i=0; i<args->mcmpals; i++)
+    for (i=0; i<ca->mcmpals; i++)
     {
-        cmpals_t *cmpals = args->cmpals + i;
+        cmpals1_t *cmpals = ca->cmpals + i;
         free(cmpals->ref);
         free(cmpals->alt);
         if ( cmpals->hash ) khash_str2int_destroy_free(cmpals->hash);
     }
-    free(args->cmpals);
+    free(ca->cmpals);
 }
 
 static void flush_buffer(args_t *args, htsFile *file, int n)
@@ -1632,17 +1637,17 @@ static void flush_buffer(args_t *args, htsFile *file, int n)
                 if ( args->rmdup & BCF_SR_PAIR_ANY ) continue;    // rmdup by position only
                 if ( args->rmdup & BCF_SR_PAIR_SNPS && line_type&(VCF_SNP|VCF_MNP) && prev_type&(VCF_SNP|VCF_MNP) ) continue;
                 if ( args->rmdup & BCF_SR_PAIR_INDELS && line_type&(VCF_INDEL) && prev_type&(VCF_INDEL) ) continue;
-                if ( args->rmdup & BCF_SR_PAIR_EXACT && cmpals_match(args, args->lines[k]) ) continue;
+                if ( args->rmdup & BCF_SR_PAIR_EXACT && cmpals_match(&args->cmpals_out, args->lines[k]) ) continue;
             }
             else
             {
                 prev_rid  = args->lines[k]->rid;
                 prev_pos  = args->lines[k]->pos;
                 prev_type = 0;
-                if ( args->rmdup & BCF_SR_PAIR_EXACT ) cmpals_reset(args);
+                if ( args->rmdup & BCF_SR_PAIR_EXACT ) cmpals_reset(&args->cmpals_out);
             }
             prev_type |= line_type;
-            if ( args->rmdup & BCF_SR_PAIR_EXACT ) cmpals_add(args, args->lines[k]);
+            if ( args->rmdup & BCF_SR_PAIR_EXACT ) cmpals_add(&args->cmpals_out, args->lines[k]);
         }
         if ( bcf_write1(file, args->hdr, args->lines[k])!=0 ) error("[%s] Error: cannot write to %s\n", __func__,args->output_fname);
     }
@@ -1673,7 +1678,8 @@ static void init_data(args_t *args)
 
 static void destroy_data(args_t *args)
 {
-    cmpals_destroy(args);
+    cmpals_destroy(&args->cmpals_in);
+    cmpals_destroy(&args->cmpals_out);
     int i;
     for (i=0; i<args->rbuf.m; i++)
         if ( args->lines[i] ) bcf_destroy1(args->lines[i]);
@@ -1774,17 +1780,17 @@ static void normalize_vcf(args_t *args)
                 if ( args->rmdup & BCF_SR_PAIR_ANY ) continue;    // rmdup by position only
                 if ( args->rmdup & BCF_SR_PAIR_SNPS && line_type&(VCF_SNP|VCF_MNP) && prev_type&(VCF_SNP|VCF_MNP) ) continue;
                 if ( args->rmdup & BCF_SR_PAIR_INDELS && line_type&(VCF_INDEL) && prev_type&(VCF_INDEL) ) continue;
-                if ( args->rmdup & BCF_SR_PAIR_EXACT && cmpals_match(args, line) ) continue;
+                if ( args->rmdup & BCF_SR_PAIR_EXACT && cmpals_match(&args->cmpals_in, line) ) continue;
             }
             else
             {
                 prev_rid  = line->rid;
                 prev_pos  = line->pos;
                 prev_type = 0;
-                if ( args->rmdup & BCF_SR_PAIR_EXACT ) cmpals_reset(args);
+                if ( args->rmdup & BCF_SR_PAIR_EXACT ) cmpals_reset(&args->cmpals_in);
             }
             prev_type |= line_type;
-            if ( args->rmdup & BCF_SR_PAIR_EXACT ) cmpals_add(args, line);
+            if ( args->rmdup & BCF_SR_PAIR_EXACT ) cmpals_add(&args->cmpals_in, line);
         }
 
         // still on the same chromosome?

test/norm.rmdup.2.1.out
@@ -1,0 +1,14 @@
+##fileformat=VCFv4.2
+##FILTER=<ID=PASS,Description="All filters passed">
+##INFO=<ID=END,Number=1,Type=Integer,Description="End position of the variant described in this record">
+##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+##contig=<ID=22,length=2147483647>
+##INFO=<ID=AC,Number=A,Type=Integer,Description="Allele count in genotypes">
+##INFO=<ID=AN,Number=1,Type=Integer,Description="Total number of alleles in called genotypes">
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	HG00096	HG00097	HG00099	HG00100	HG00101	HG00102	HG00103	HG00105	HG00106	HG00107
+22	16854058	.	C	G	.	.	AC=0;AN=20	GT	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0
+22	16854058	.	C	T	.	.	AC=0;AN=20	GT	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0
+22	16897908	.	C	A	.	.	AC=0;AN=20	GT	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0
+22	16897908	.	C	T	.	.	AC=2;AN=20	GT	0|0	0|1	0|0	0|0	0|0	0|0	0|0	0|0	1|0	0|0
+22	16930571	.	A	C	.	.	AC=0;AN=20	GT	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0
+22	16930571	.	A	G	.	.	AC=0;AN=20	GT	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0

test/norm.rmdup.2.2.out
@@ -1,0 +1,11 @@
+##fileformat=VCFv4.2
+##FILTER=<ID=PASS,Description="All filters passed">
+##INFO=<ID=END,Number=1,Type=Integer,Description="End position of the variant described in this record">
+##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+##contig=<ID=22,length=2147483647>
+##INFO=<ID=AC,Number=A,Type=Integer,Description="Allele count in genotypes">
+##INFO=<ID=AN,Number=1,Type=Integer,Description="Total number of alleles in called genotypes">
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	HG00096	HG00097	HG00099	HG00100	HG00101	HG00102	HG00103	HG00105	HG00106	HG00107
+22	16854058	.	C	G	.	.	AC=0;AN=20	GT	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0
+22	16897908	.	C	A	.	.	AC=0;AN=20	GT	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0
+22	16930571	.	A	C	.	.	AC=0;AN=20	GT	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0

test/norm.rmdup.2.vcf
@@ -1,0 +1,14 @@
+##fileformat=VCFv4.2
+##FILTER=<ID=PASS,Description="All filters passed">
+##INFO=<ID=END,Number=1,Type=Integer,Description="End position of the variant described in this record">
+##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+##contig=<ID=22,length=2147483647>
+##INFO=<ID=AC,Number=A,Type=Integer,Description="Allele count in genotypes">
+##INFO=<ID=AN,Number=1,Type=Integer,Description="Total number of alleles in called genotypes">
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	HG00096	HG00097	HG00099	HG00100	HG00101	HG00102	HG00103	HG00105	HG00106	HG00107
+22	16854058	.	C	G	.	.	AC=0;AN=20	GT	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0
+22	16854058	.	C	T	.	.	AC=0;AN=20	GT	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0
+22	16897908	.	C	A	.	.	AC=0;AN=20	GT	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0
+22	16897908	.	C	T	.	.	AC=2;AN=20	GT	0|0	0|1	0|0	0|0	0|0	0|0	0|0	0|0	1|0	0|0
+22	16930571	.	A	C	.	.	AC=0;AN=20	GT	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0
+22	16930571	.	A	G	.	.	AC=0;AN=20	GT	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0	0|0

test/test.pl
@@ -161,6 +161,11 @@ test_vcf_norm($opts,in=>'norm.rmdup',out=>'norm.rmdup.2.out',args=>'-d indels');
 test_vcf_norm($opts,in=>'norm.rmdup',out=>'norm.rmdup.3.out',args=>'-d both');
 test_vcf_norm($opts,in=>'norm.rmdup',out=>'norm.rmdup.4.out',args=>'-d all');
 test_vcf_norm($opts,in=>'norm.rmdup',out=>'norm.rmdup.5.out',args=>'-d none');
+test_vcf_norm($opts,in=>'norm.rmdup.2',out=>'norm.rmdup.2.1.out',args=>'-d none');
+test_vcf_norm($opts,in=>'norm.rmdup.2',out=>'norm.rmdup.2.1.out',args=>'-d indels');
+test_vcf_norm($opts,in=>'norm.rmdup.2',out=>'norm.rmdup.2.2.out',args=>'-d any');
+test_vcf_norm($opts,in=>'norm.rmdup.2',out=>'norm.rmdup.2.2.out',args=>'-d both');
+test_vcf_norm($opts,in=>'norm.rmdup.2',out=>'norm.rmdup.2.2.out',args=>'-d snps');
 test_vcf_view($opts,in=>'view',out=>'view.1.out',args=>'-aUc1 -C1 -s NA00002 -v snps',reg=>'');
 test_vcf_view($opts,in=>'view',out=>'view.2.out',args=>'-f PASS -Xks NA00003',reg=>'-r20,Y');
 test_vcf_view($opts,in=>'view',out=>'view.3.out',args=>'-xs NA00003',reg=>'');