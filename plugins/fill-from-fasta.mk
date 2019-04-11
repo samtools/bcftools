079476d5aeab375b5aa33c33ed00cda7ea05aa01
Petr Danecek <pd3@sanger.ac.uk>
2019-04-11 08:27:56 +0100

    Remove unnecessary .mk files
    and declare `bcf_double_missing` and `bcf_double_vector_end` in filter.c
    as const. This builds on 8858ae07

--- plugins/fill-from-fasta.mk
+++
@@ -1,2 +0,0 @@
-plugins/fill-from-fasta.so: plugins/fill-from-fasta.c version.h version.c filter.h filter.c
-	$(CC) $(PLUGIN_FLAGS) $(CFLAGS) $(ALL_CPPFLAGS) $(EXTRA_CPPFLAGS) $(PERL_CFLAGS) $(LDFLAGS) -o $@ filter.c version.c $< $(LIBS)
