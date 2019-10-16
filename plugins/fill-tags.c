samtools/bcftools

commit 3e2d79e4de1d3655d2af26b8bf3efba3394d890a

Support for simple fill-tags expressions such as "-t DP=sum(DP)"

plugins/fill-tags.c
@@ -1,6 +1,6 @@
 /* The MIT License
 
-   Copyright (c) 2015 Genome Research Ltd.
+   Copyright (c) 2015-2019 Genome Research Ltd.
 
    Author: Petr Danecek <pd3@sanger.ac.uk>
    
@@ -47,6 +47,17 @@
 #define SET_MAF     (1<<7)
 #define SET_HWE     (1<<8)
 #define SET_ExcHet  (1<<9)
+#define SET_FUNC    (1<<10)
+
+typedef struct _args_t args_t;
+typedef struct _ftf_t ftf_t;
+typedef int (*fill_tag_f)(args_t *, bcf1_t *, ftf_t *);
+struct _ftf_t
+{
+    char *src_tag, *dst_tag;
+    fill_tag_f func;
+    int *pop_vals;      // for now assuming only 1 integer value per annotation
+};
 
 typedef struct
 {
@@ -64,7 +75,7 @@ typedef struct
 }
 pop_t;
 
-typedef struct
+struct _args_t
 {
     bcf_hdr_t *in_hdr, *out_hdr;
     int npop, tags, drop_missing, gt_id;
@@ -75,21 +86,23 @@ typedef struct
     int mhwe_probs;
     kstring_t str;
     kbitset_t *bset;
-}
-args_t;
+    ftf_t *ftf;
+    int nftf;
+};
 
 static args_t *args;
 
 const char *about(void)
 {
-    return "Set INFO tags AF, AC, AC_Hemi, AC_Hom, AC_Het, AN, ExcHet, HWE, MAF, NS.\n";
+    return "Set INFO tags AF, AC, AC_Hemi, AC_Hom, AC_Het, AN, ExcHet, HWE, MAF, NS and more.\n";
 }
 
 const char *usage(void)
 {
     return 
         "\n"
-        "About: Set INFO tags AF, AC, AC_Hemi, AC_Hom, AC_Het, AN, ExcHet, HWE, MAF, NS.\n"
+        "About: Set INFO tags AF, AC, AC_Hemi, AC_Hom, AC_Het, AN, ExcHet, HWE, MAF, NS\n"
+        "   or custom INFO/TAG=func(FMT/TAG), use -l for detailed description\n"
         "Usage: bcftools +fill-tags [General Options] -- [Plugin Options]\n"
         "Options:\n"
         "   run \"bcftools plugin\" for a list of common options\n"
@@ -97,14 +110,24 @@ const char *usage(void)
         "Plugin options:\n"
         "   -d, --drop-missing          do not count half-missing genotypes \"./1\" as hemizygous\n"
         "   -l, --list-tags             list available tags with description\n"
-        "   -t, --tags LIST             list of output tags. By default, all tags are filled.\n"
+        "   -t, --tags LIST             list of output tags, \"all\" for all tags\n"
         "   -S, --samples-file FILE     list of samples (first column) and comma-separated list of populations (second column)\n"
         "\n"
         "Example:\n"
-        "   bcftools +fill-tags in.bcf -Ob -o out.bcf\n"
+        "   # Print a detailed list of available tags\n"
+        "   bcftools +fill-tags -- -l\n"
+        "\n"
+        "   # Fill INFO/AN and INFO/AC\n"
         "   bcftools +fill-tags in.bcf -Ob -o out.bcf -- -t AN,AC\n"
-        "   bcftools +fill-tags in.bcf -Ob -o out.bcf -- -d\n"
+        "\n"
+        "   # Fill all available tags\n"
+        "   bcftools +fill-tags in.bcf -Ob -o out.bcf -- -t all\n"
+        "\n"
+        "   # Calculate HWE for sample groups (possibly multiple) read from a file\n"
         "   bcftools +fill-tags in.bcf -Ob -o out.bcf -- -S sample-group.txt -t HWE\n"
+        "\n"
+        "   # Calculate total read depth (INFO/DP) from per-sample depths (FORMAT/DP)\n"
+        "   bcftools +fill-tags in.bcf -Ob -o out.bcf -- -t 'DP=sum(DP)'\n"
         "\n";
 }
 
@@ -214,13 +237,123 @@ void init_pops(args_t *args)
     }
 }
 
+void ftf_destroy(args_t *args)
+{
+    int i;
+    for (i=0; i<args->nftf; i++)
+    {
+        ftf_t *ftf = &args->ftf[i];
+        free(ftf->src_tag);
+        free(ftf->dst_tag);
+        free(ftf->pop_vals);
+    }
+    free(args->ftf);
+}
+int ftf_sum(args_t *args, bcf1_t *rec, ftf_t *ftf)
+{
+    int nsmpl = bcf_hdr_nsamples(args->in_hdr);
+    int nval = bcf_get_format_int32(args->in_hdr, rec, ftf->src_tag, &args->iarr, &args->niarr);
+    if ( nval<=0 ) return 0;
+    nval /= nsmpl;
+
+    int i;
+    for (i=0; i<args->npop; i++)
+        ftf->pop_vals[i] = -1;
+
+    for (i=0; i<nsmpl; i++)
+    {
+        if ( args->iarr[i*nval]==bcf_int32_missing || args->iarr[i*nval]==bcf_int32_vector_end ) continue;
+
+        pop_t **pop = &args->smpl2pop[i*(args->npop+1)];
+        while ( *pop )
+        {
+            int ipop = (int)(*pop - args->pop);
+            if ( ftf->pop_vals[ipop]<0 ) ftf->pop_vals[ipop] = 0;
+            ftf->pop_vals[ipop] += args->iarr[i*nval];
+            pop++;
+        }
+    }
+
+    for (i=0; i<args->npop; i++)
+    {
+        if ( ftf->pop_vals[i]<0 ) continue;
+        args->str.l = 0;
+        ksprintf(&args->str, "%s%s", ftf->dst_tag,args->pop[i].suffix);
+        if ( bcf_update_info_int32(args->out_hdr,rec,args->str.s,ftf->pop_vals+i,1)!=0 )
+            error("Error occurred while updating %s at %s:%"PRId64"\n", args->str.s,bcf_seqname(args->in_hdr,rec),(int64_t) rec->pos+1);
+    }
+
+    return 0;
+}
+
+void hdr_append(args_t *args, char *fmt)
+{
+    int i;
+    for (i=0; i<args->npop; i++)
+        bcf_hdr_printf(args->out_hdr, fmt, args->pop[i].suffix,*args->pop[i].name ? " in " : "",args->pop[i].name);
+}
+
+int parse_func(args_t *args, char *tag, char *expr)
+{
+    args->nftf++;
+    args->ftf = (ftf_t *)realloc(args->ftf,sizeof(*args->ftf)*args->nftf);
+    ftf_t *ftf = &args->ftf[ args->nftf - 1 ];
+
+    ftf->pop_vals = (int*)calloc(args->npop,sizeof(*ftf->pop_vals));
+    ftf->dst_tag = (char*)calloc(expr-tag,1);
+    memcpy(ftf->dst_tag, tag, expr-tag-1);
+
+    if ( !strncasecmp(expr,"sum(",4) ) { ftf->func  = ftf_sum; expr += 4; }
+    else error("Error: the expression not recognised: %s\n",tag);
+
+    char *tmp = expr; 
+    while ( *tmp && *tmp!=')' ) tmp++;
+    if ( !*tmp ) error("Error: could not parse: %s\n",tag);
+
+    ftf->src_tag = (char*)calloc(tmp-expr+2,1);
+    memcpy(ftf->src_tag, expr, tmp-expr);
+
+    int id = bcf_hdr_id2int(args->in_hdr,BCF_DT_ID,ftf->src_tag);
+    if ( !bcf_hdr_idinfo_exists(args->in_hdr,BCF_HL_FMT,id) ) error("Error: the field FORMAT/%s is not present\n",ftf->src_tag);
+
+    int i = 0;
+    for (i=0; i<args->npop; i++)
+    {
+        args->str.l = 0;
+        ksprintf(&args->str, "%s%s", ftf->dst_tag,args->pop[i].suffix);
+        id = bcf_hdr_id2int(args->out_hdr,BCF_DT_ID,args->str.s);
+        if ( bcf_hdr_idinfo_exists(args->out_hdr,BCF_HL_FMT,id) )
+        {
+            if ( bcf_hdr_id2length(args->out_hdr,BCF_HL_FMT,id)!=BCF_VL_FIXED )
+                error("Error: the field INFO/%s already exists with a definition different from Number=1\n",args->str.s);
+            if ( bcf_hdr_id2number(args->out_hdr,BCF_HL_FMT,id)!=1 )
+                error("Error: the field INFO/%s already exists with a definition different from Number=1\n",args->str.s);
+            if ( bcf_hdr_id2type(args->out_hdr,BCF_HT_INT,id)!=BCF_HT_INT )
+                error("Error: the field INFO/%s already exists with a definition different from Type=Integer\n",args->str.s);
+        }
+        else
+        {   
+            kstring_t str = {0,0,0};
+            ksprintf(&str,"##INFO=<ID=%s%%s,Number=1,Type=Integer,Description=\"%s\">",args->str.s,tag);
+            hdr_append(args, str.s);
+            free(str.s);
+        }
+    }
+    return SET_FUNC;
+}
 int parse_tags(args_t *args, const char *str)
 {
-    int i, flag = 0, n_tags;
-    char **tags = hts_readlist(str, 0, &n_tags);
+    if ( !args->in_hdr ) error("%s", usage());
+
+    int i,j, flag = 0, n_tags;
+    char **tags = hts_readlist(str, 0, &n_tags), *ptr;
     for(i=0; i<n_tags; i++)
     {
-        if ( !strcasecmp(tags[i],"AN") ) flag |= SET_AN;
+        if ( !strcasecmp(tags[i],"all") )
+        {
+            for (j=0; j<=10; j++) args->tags |= 1<<j;
+        }
+        else if ( !strcasecmp(tags[i],"AN") ) flag |= SET_AN;
         else if ( !strcasecmp(tags[i],"AC") ) flag |= SET_AC;
         else if ( !strcasecmp(tags[i],"NS") ) flag |= SET_NS;
         else if ( !strcasecmp(tags[i],"AC_Hom") ) flag |= SET_AC_Hom;
@@ -230,6 +363,7 @@ int parse_tags(args_t *args, const char *str)
         else if ( !strcasecmp(tags[i],"MAF") ) flag |= SET_MAF;
         else if ( !strcasecmp(tags[i],"HWE") ) flag |= SET_HWE;
         else if ( !strcasecmp(tags[i],"ExcHet") ) flag |= SET_ExcHet;
+        else if ( (ptr=strchr(tags[i],'=')) ) flag |= parse_func(args,tags[i],ptr+1);
         else
         {
             fprintf(stderr,"Error parsing \"--tags %s\": the tag \"%s\" is not supported\n", str,tags[i]);
@@ -239,13 +373,6 @@ int parse_tags(args_t *args, const char *str)
     }
     if (n_tags) free(tags);
     return flag;
-}
-
-void hdr_append(args_t *args, char *fmt)
-{
-    int i;
-    for (i=0; i<args->npop; i++)
-        bcf_hdr_printf(args->out_hdr, fmt, args->pop[i].suffix,*args->pop[i].name ? " in " : "",args->pop[i].name);
 }
 
 void list_tags(void)
@@ -261,6 +388,8 @@ void list_tags(void)
         "INFO/MAF      Number:A  Type:Float    ..  Minor Allele frequency\n"
         "INFO/HWE      Number:A  Type:Float    ..  HWE test (PMID:15789306); 1=good, 0=bad\n"
         "INFO/ExcHet   Number:A  Type:Float    ..  Test excess heterozygosity; 1=good, 0=bad\n"
+        "TAG=func(TAG) Number:1  Type:Integer  ..  Experimental support for user-defined\n"
+        "    expressions such as \"DP=sum(DP)\". This is currently very basic, to be extended.\n"
         );
 }
 
@@ -269,7 +398,7 @@ int init(int argc, char **argv, bcf_hdr_t *in, bcf_hdr_t *out)
     args = (args_t*) calloc(1,sizeof(args_t));
     args->in_hdr  = in;
     args->out_hdr = out;
-    char *samples_fname = NULL;
+    char *samples_fname = NULL, *tags_str = "all";
     static struct option loptions[] =
     {
         {"list-tags",0,0,'l'},
@@ -285,7 +414,7 @@ int init(int argc, char **argv, bcf_hdr_t *in, bcf_hdr_t *out)
         {
             case 'l': list_tags(); break;
             case 'd': args->drop_missing = 1; break;
-            case 't': args->tags |= parse_tags(args,optarg); break;
+            case 't': tags_str = optarg; break;
             case 'S': samples_fname = optarg; break;
             case 'h':
             case '?':
@@ -298,11 +427,10 @@ int init(int argc, char **argv, bcf_hdr_t *in, bcf_hdr_t *out)
     args->gt_id = bcf_hdr_id2int(args->in_hdr,BCF_DT_ID,"GT");
     if ( args->gt_id<0 ) error("Error: GT field is not present\n");
 
-    if ( !args->tags )
-        for (c=0; c<=9; c++) args->tags |= 1<<c;    // by default all tags will be filled
-
     if ( samples_fname ) parse_samples(args, samples_fname);
     init_pops(args);
+
+    args->tags |= parse_tags(args,tags_str);
 
     if ( args->tags & SET_AN ) hdr_append(args, "##INFO=<ID=AN%s,Number=1,Type=Integer,Description=\"Total number of alleles in called genotypes%s%s\">");
     if ( args->tags & SET_AC ) hdr_append(args, "##INFO=<ID=AC%s,Number=A,Type=Integer,Description=\"Allele count in genotypes%s%s\">");
@@ -414,9 +542,13 @@ static void clean_counts(pop_t *pop, int nals)
 
 bcf1_t *process(bcf1_t *rec)
 {
+    bcf_unpack(rec, BCF_UN_FMT);
+
     int i,j, nsmpl = bcf_hdr_nsamples(args->in_hdr);;
 
-    bcf_unpack(rec, BCF_UN_FMT);
+    for (i=0; i<args->nftf; i++)
+        args->ftf[i].func(args, rec, &args->ftf[i]);
+
     bcf_fmt_t *fmt_gt = NULL;
     for (i=0; i<rec->n_fmt; i++)
         if ( rec->d.fmt[i].id==args->gt_id ) { fmt_gt = &rec->d.fmt[i]; break; }

test/fill-tags.5.out
@@ -1,0 +1,53 @@
+##fileformat=VCFv4.1
+##FILTER=<ID=PASS,Description="All filters passed">
+##reference=file:///seq/references/1000Genomes-NCBI37.fasta
+##contig=<ID=11,length=135006516>
+##contig=<ID=20,length=63025520>
+##contig=<ID=X,length=155270560>
+##contig=<ID=Y,length=59373566>
+##INFO=<ID=DP,Number=1,Type=Integer,Description="Raw read depth">
+##INFO=<ID=DP4,Number=4,Type=Integer,Description="# high-quality ref-forward bases, ref-reverse, alt-forward and alt-reverse bases">
+##INFO=<ID=Dels,Number=1,Type=Float,Description="Fraction of reads containing spanning deletions">
+##INFO=<ID=FS,Number=1,Type=Float,Description="Phred-scaled p-value using Fisher's exact test to detect strand bias">
+##INFO=<ID=HRun,Number=1,Type=Integer,Description="Largest contiguous homopolymer run of variant allele in either direction">
+##INFO=<ID=HWE,Number=1,Type=Float,Description="Hardy-Weinberg equilibrium test (PMID:15789306)">
+##INFO=<ID=ICF,Number=1,Type=Float,Description="Inbreeding coefficient F">
+##INFO=<ID=INDEL,Number=0,Type=Flag,Description="Indicates that the variant is an INDEL.">
+##INFO=<ID=IS,Number=2,Type=Float,Description="Maximum number of reads supporting an indel and fraction of indel reads">
+##INFO=<ID=MQ,Number=1,Type=Integer,Description="Root-mean-square mapping quality of covering reads">
+##INFO=<ID=MQ0,Number=1,Type=Integer,Description="Total mapping quality zero reads">
+##INFO=<ID=PV4,Number=4,Type=Float,Description="P-values for strand bias, baseQ bias, mapQ bias and tail distance bias">
+##INFO=<ID=QD,Number=1,Type=Float,Description="Variant confidence/quality by depth">
+##INFO=<ID=AC,Number=A,Type=Integer,Description="Allele count in genotypes">
+##INFO=<ID=AN,Number=1,Type=Integer,Description="Total number of alleles in called genotypes">
+##FORMAT=<ID=DP,Number=1,Type=Integer,Description="# high-quality bases">
+##FORMAT=<ID=GQ,Number=1,Type=Integer,Description="Genotype Quality">
+##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+##FORMAT=<ID=PL,Number=G,Type=Integer,Description="List of Phred-scaled genotype likelihoods">
+##FILTER=<ID=StrandBias,Description="Min P-value for strand bias (INFO/PV4) [0.0001]">
+##FILTER=<ID=BaseQualBias,Description="Min P-value for baseQ bias (INFO/PV4) [1e-100]">
+##FILTER=<ID=MapQualBias,Description="Min P-value for mapQ bias (INFO/PV4) [0]">
+##FILTER=<ID=EndDistBias,Description="Min P-value for end distance bias (INFO/PV4) [0.0001]">
+##FILTER=<ID=MinAB,Description="Minimum number of alternate bases (INFO/DP4) [2]">
+##INFO=<ID=DP_AB_AB,Number=1,Type=Integer,Description="DP=sum(DP)">
+##INFO=<ID=DP_AB_BC,Number=1,Type=Integer,Description="DP=sum(DP)">
+##INFO=<ID=DP_AB,Number=1,Type=Integer,Description="DP=sum(DP)">
+##INFO=<ID=DP_BC_AB,Number=1,Type=Integer,Description="DP=sum(DP)">
+##INFO=<ID=DP_BC_BC,Number=1,Type=Integer,Description="DP=sum(DP)">
+##INFO=<ID=DP_BC,Number=1,Type=Integer,Description="DP=sum(DP)">
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	NA00001	NA00002	NA00003
+11	2343543	.	A	.	999	PASS	DP=586;DP_AB=404;DP_BC=393	GT:PL:DP:GQ	0/0:0:193:99	0/0:0:211:99	0/0:0:182:99
+11	5464562	.	C	T	999	PASS	DP=0	GT:PL:DP:GQ	./.:0,0,0:.:.	./.:0,0,0:.:.	./.:0,0,0:.:.
+20	76962	rs6111385	T	C	999	PASS	DP4=110138,70822,421911,262673;DP=586;Dels=0;FS=21.447;HWE=0.491006;ICF=-0.01062;MQ0=1;MQ=46;PV4=2.5e-09,0,0,1;QD=22.31;DP_AB=404;DP_BC=393	GT:PL:DP:GQ	0/1:255,0,255:193:99	1/1:255,255,0:211:99	1/1:255,255,0:182:99
+20	126310	.	ACC	A	999	StrandBias;EndDistBias	DP4=125718,95950,113812,80890;DP=306;HWE=0.24036;ICF=0.01738;INDEL;IS=374,0.937343;MQ=49;PV4=9e-30,1,0,3.8e-13;QD=0.0172;AN=6;AC=4;DP_AB=228;DP_BC=189	GT:DP:GQ:PL	0/1:117:99:255,0,132	0/1:111:99:255,0,139	1/1:78:99:255,213,0
+20	138125	rs2298108	G	T	999	PASS	DP4=174391,20849,82080,4950;DP=203;Dels=0;FS=3200;HWE=0.199462;ICF=0.01858;MQ0=0;MQ=46;PV4=0,0,0,1;QD=17.22;AN=6;AC=4;DP_AB=137;DP_BC=137	GT:PL:DP:GQ	0/1:135,0,163:66:99	0/1:140,0,255:71:99	1/1:255,199,0:66:99
+20	138148	rs2298109	C	T	999	PASS	DP4=194136,45753,94945,14367;DP=247;Dels=0;FS=3200;HWE=0.177865;ICF=0.0198;MQ0=0;MQ=47;PV4=0,0,0,1;QD=14.57;AN=6;AC=4;DP_AB=169;DP_BC=160	GT:PL:DP:GQ	0/1:195,0,255:87:99	0/1:192,0,255:82:99	1/1:255,235,0:78:99
+20	271225	.	T	TTTA,TA	999	StrandBias	DP4=29281,42401,27887,29245;DP=131;INDEL;IS=95,0.748031;MQ=47;PV4=0,1,0,1;QD=0.0948;AN=6;AC=2,2;DP_AB=84;DP_BC=98	GT:DP:GQ:PL	0/2:33:49:151,53,203,0,52,159	0/1:51:99:255,0,213,255,255,255	1/2:47:99:255,255,255,255,0,241
+20	304568	.	C	T	999	PASS	DP4=16413,4543,945,156;DP=163;Dels=0;FS=3200;HWE=0.076855;ICF=0.0213;MQ0=0;MQ=50;PV4=0,0,0,1;QD=15.45;AN=6;AC=4;DP_AB=103;DP_BC=73	GT:PL:DP:GQ	0|1:95,0,255:90:99	0|1:192,0,255:13:99	1|1:255,95,0:60:99
+20	326891	.	A	AC	999	PASS	DP4=125718,95950,113812,80890;DP=228;HWE=0.24036;ICF=0.01738;INDEL;IS=374,0.937343;MQ=49;PV4=9e-30,1,0,3.8e-13;QD=0.0172;AN=4;AC=2;DP_AB=228;DP_BC=111	GT:DP:GQ:PL	0|1:117:99:255,0,132	0|1:111:99:255,0,139	./.:.:.:.,.,.
+X	2928329	rs62584840	C	T	999	PASS	DP4=302,9137,32,1329;DP=9;Dels=0;FS=13.38;HWE=0.284332;ICF=0.0253;MQ0=0;MQ=49;PV4=0.094,0,0,1;QD=18.61;AN=4;AC=1;DP_AB=5;DP_BC=7	GT:PL:DP:GQ	0:0,56:2:73	0:0,81:3:98	0/1:73,0,19:4:30
+X	2933066	rs61746890	G	C	999	PASS	DP4=69865,100561,461,783;DP=138;Dels=0;FS=10.833;MQ0=0;MQ=50;PV4=0.005,3.6e-14,0,1;QD=15.33;AN=4;AC=1;DP_AB=76;DP_BC=99	GT:PL:DP:GQ	0:0,255:39:99	0:0,255:37:99	0/1:255,255,255:62:99
+X	2942109	rs5939407	T	C	999	PASS	DP4=23273,27816,40128,48208;DP=105;Dels=0;FS=43.639;HWE=0.622715;ICF=-0.01176;MQ0=1;MQ=46;PV4=0.65,1,0,1;QD=14.81;AN=4;AC=3;DP_AB=53;DP_BC=85	GT:PL:DP:GQ	0:0,255:20:99	1:255,0:33:99	1/1:255,157,0:52:99
+X	3048719	.	T	C	999	PASS	DP4=13263,27466,40128,48208;DP=105;Dels=0;FS=43.639;HWE=0.622715;ICF=-0.01176;MQ0=1;MQ=46;PV4=0.65,1,0,1;QD=14.81;AN=4;AC=3;DP_AB=53;DP_BC=85	GT:PL:DP:GQ	0:0,255:20:99	1:255,0:33:99	0|1:255,0,157:52:99
+Y	8657215	.	C	A	999	PASS	DP4=74915,114274,1948,2955;DP=111;Dels=0;FS=3.181;MQ0=0;MQ=50;PV4=0.86,1,0,1;QD=33.77;AN=2;AC=1;DP_AB=111;DP_BC=64	GT:PL:DP:GQ	0:0,255:47:99	1:255,0:64:99	.:.:.:.
+Y	10011673	rs78249411	G	A	999	MinAB	DP4=47351,30839,178796,279653;DP=276;Dels=0;FS=41.028;MQ0=37362;MQ=26;PV4=0,0,0,1;QD=17.45;AN=2;AC=2;DP_AB=276;DP_BC=130	GT:PL:DP:GQ	1:126,101:146:37	1:95,0:130:99	.:.:.:.

test/test.pl
@@ -351,6 +351,7 @@ test_vcf_plugin($opts,in=>'query.nucleotide',out=>'query.allele-length.tsv',cmd=
 test_vcf_plugin($opts,in=>'merge.a',out=>'fill-tags.out',cmd=>'+fill-tags --no-version',args=>'-- -t AN,AC,AC_Hom,AC_Het,AC_Hemi');
 test_vcf_plugin($opts,in=>'view',out=>'fill-tags.2.out',cmd=>'+fill-tags --no-version',args=>'-- -t AC,AN,AF,MAF,NS');
 test_vcf_plugin($opts,in=>'view',out=>'fill-tags.3.out',cmd=>'+fill-tags --no-version',args=>'-- -t AC -S {PATH}/fill-tags.3.smpl');
+test_vcf_plugin($opts,in=>'view',out=>'fill-tags.5.out',cmd=>'+fill-tags --no-version',args=>'-- -t "DP=sum(DP)" -S {PATH}/fill-tags.3.smpl');
 test_vcf_plugin($opts,in=>'many-alts',out=>'fill-tags.4.out',cmd=>'+fill-tags --no-version',args=>'-- -t AN,AC');
 test_vcf_plugin($opts,in=>'fill-tags-hemi',out=>'fill-tags-hemi.1.out',cmd=>'+fill-tags --no-version');
 test_vcf_plugin($opts,in=>'fill-tags-hemi',out=>'fill-tags-hemi.2.out',cmd=>'+fill-tags --no-version',args=>'-- -d');

commit 3607a35e42b29ee563f6890168fe9fa70770caec

Fix two bugs introduced with 3e2d79e4: header formatting and pass the correct buffer size vaariable

plugins/fill-tags.c
@@ -252,7 +252,7 @@ void ftf_destroy(args_t *args)
 int ftf_sum(args_t *args, bcf1_t *rec, ftf_t *ftf)
 {
     int nsmpl = bcf_hdr_nsamples(args->in_hdr);
-    int nval = bcf_get_format_int32(args->in_hdr, rec, ftf->src_tag, &args->iarr, &args->niarr);
+    int nval = bcf_get_format_int32(args->in_hdr, rec, ftf->src_tag, &args->iarr, &args->miarr);
     if ( nval<=0 ) return 0;
     nval /= nsmpl;
 
@@ -321,23 +321,18 @@ int parse_func(args_t *args, char *tag, char *expr)
     {
         args->str.l = 0;
         ksprintf(&args->str, "%s%s", ftf->dst_tag,args->pop[i].suffix);
-        id = bcf_hdr_id2int(args->out_hdr,BCF_DT_ID,args->str.s);
-        if ( bcf_hdr_idinfo_exists(args->out_hdr,BCF_HL_FMT,id) )
+        id = bcf_hdr_id2int(args->in_hdr,BCF_DT_ID,args->str.s);
+        if ( bcf_hdr_idinfo_exists(args->in_hdr,BCF_HL_FMT,id) )
         {
-            if ( bcf_hdr_id2length(args->out_hdr,BCF_HL_FMT,id)!=BCF_VL_FIXED )
+            if ( bcf_hdr_id2length(args->in_hdr,BCF_HL_FMT,id)!=BCF_VL_FIXED )
                 error("Error: the field INFO/%s already exists with a definition different from Number=1\n",args->str.s);
-            if ( bcf_hdr_id2number(args->out_hdr,BCF_HL_FMT,id)!=1 )
+            if ( bcf_hdr_id2number(args->in_hdr,BCF_HL_FMT,id)!=1 )
                 error("Error: the field INFO/%s already exists with a definition different from Number=1\n",args->str.s);
-            if ( bcf_hdr_id2type(args->out_hdr,BCF_HT_INT,id)!=BCF_HT_INT )
+            if ( bcf_hdr_id2type(args->in_hdr,BCF_HT_INT,id)!=BCF_HT_INT )
                 error("Error: the field INFO/%s already exists with a definition different from Type=Integer\n",args->str.s);
         }
         else
-        {   
-            kstring_t str = {0,0,0};
-            ksprintf(&str,"##INFO=<ID=%s%%s,Number=1,Type=Integer,Description=\"%s\">",args->str.s,tag);
-            hdr_append(args, str.s);
-            free(str.s);
-        }
+            bcf_hdr_printf(args->out_hdr, "##INFO=<ID=%s,Number=1,Type=Integer,Description=\"%s%s%s\">",args->str.s,tag,*args->pop[i].name ? " in " : "",args->pop[i].name);
     }
     return SET_FUNC;
 }

test/fill-tags.5.out
@@ -29,12 +29,8 @@
 ##FILTER=<ID=MapQualBias,Description="Min P-value for mapQ bias (INFO/PV4) [0]">
 ##FILTER=<ID=EndDistBias,Description="Min P-value for end distance bias (INFO/PV4) [0.0001]">
 ##FILTER=<ID=MinAB,Description="Minimum number of alternate bases (INFO/DP4) [2]">
-##INFO=<ID=DP_AB_AB,Number=1,Type=Integer,Description="DP=sum(DP)">
-##INFO=<ID=DP_AB_BC,Number=1,Type=Integer,Description="DP=sum(DP)">
-##INFO=<ID=DP_AB,Number=1,Type=Integer,Description="DP=sum(DP)">
-##INFO=<ID=DP_BC_AB,Number=1,Type=Integer,Description="DP=sum(DP)">
-##INFO=<ID=DP_BC_BC,Number=1,Type=Integer,Description="DP=sum(DP)">
-##INFO=<ID=DP_BC,Number=1,Type=Integer,Description="DP=sum(DP)">
+##INFO=<ID=DP_AB,Number=1,Type=Integer,Description="DP=sum(DP) in AB">
+##INFO=<ID=DP_BC,Number=1,Type=Integer,Description="DP=sum(DP) in BC">
 #CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	NA00001	NA00002	NA00003
 11	2343543	.	A	.	999	PASS	DP=586;DP_AB=404;DP_BC=393	GT:PL:DP:GQ	0/0:0:193:99	0/0:0:211:99	0/0:0:182:99
 11	5464562	.	C	T	999	PASS	DP=0	GT:PL:DP:GQ	./.:0,0,0:.:.	./.:0,0,0:.:.	./.:0,0,0:.:.