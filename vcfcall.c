samtools/bcftools

commit 12a6617a0b571c1a8b9903a9f75975a232c1257c

Exit with an informative error message when wrong format given. Resolves #2095

vcfcall.c
@@ -397,7 +397,8 @@ static int tgt_parse(const char *line, char **chr_beg, char **chr_end, uint32_t 
     while ( *ss )
     {
         se = ss;
-        while ( *se && *se!=',' ) se++;
+        while ( *se && *se!=',' && !isspace(*se) ) se++;
+        if ( !*se || isspace(*se) ) break;
         als->n++;
         als->allele = (char**)realloc(als->allele,als->n*sizeof(*als->allele));
         als->allele[als->n-1] = (char*)malloc(se-ss+1);
@@ -406,6 +407,7 @@ static int tgt_parse(const char *line, char **chr_beg, char **chr_end, uint32_t 
         ss = se+1;
         if ( !*se ) break;
     }
+    if ( als->n<2 ) error("Unable to parse the -T file; expected CHROM\\tPOS\\tREF,ALT with -C alleles but found instead:\n\t%s\n",line);
     return 0;
 }
 static void tgt_free(void *payload)

commit 9bfaa6dfec44e328951ddd76eca142d6036f68f7

Fix a silly bug introduced by 12a6617a0b571c1a8b9903a9f75975a232c1257c

vcfcall.c
@@ -398,14 +398,13 @@ static int tgt_parse(const char *line, char **chr_beg, char **chr_end, uint32_t 
     {
         se = ss;
         while ( *se && *se!=',' && !isspace(*se) ) se++;
-        if ( !*se || isspace(*se) ) break;
         als->n++;
         als->allele = (char**)realloc(als->allele,als->n*sizeof(*als->allele));
         als->allele[als->n-1] = (char*)malloc(se-ss+1);
         memcpy(als->allele[als->n-1],ss,se-ss);
         als->allele[als->n-1][se-ss] = 0;
         ss = se+1;
-        if ( !*se ) break;
+        if ( !*se || isspace(*se) ) break;
     }
     if ( als->n<2 ) error("Unable to parse the -T file; expected CHROM\\tPOS\\tREF,ALT with -C alleles but found instead:\n\t%s\n",line);
     return 0;