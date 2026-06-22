c66024665459c440c3b985e878646c4fd1eb7cb1
Rob Davies <rmd+git@sanger.ac.uk>
2026-06-22 12:10:31 +0100

    Change sort order for trio-dnm3 test file
    The trio-dnm3.1.out file was sorted using `-k4,4g`, which is an
    extension to the POSIX specification.  While widely supported,
    it isn't available everywhere leading to test failures on
    some platforms.  As the ordering isn't that important for the
    purposes of the test, the easiest solution is to sort it by
    the filename in the first column instead.

--- test/trio-dnm3/test.sh
+++ test/trio-dnm3/test.sh
@@ -3,8 +3,9 @@
 cd "$(dirname "${BASH_SOURCE[0]}")"
 
 BCFTOOLS=${1:-bcftools}
+export LC_ALL=C
 
 for x in *.vcf; do
     "$BCFTOOLS" +trio-dnm3 -p 1X:child,dad,mom $x | "$BCFTOOLS" query -uf$x'\t%CHROM:%POS\t%EXP\t[ %DNM]\t[ %VAF]\t[ %AD]\t%REF,%ALT\t[ %VA]\t[ %SP]';
-done | sort -k3,3g -k4,4g -k3,3d | sed 's,fp,-, ; s,tp,o,'
+done | sort | sed 's,fp,-, ; s,tp,o,'
 
