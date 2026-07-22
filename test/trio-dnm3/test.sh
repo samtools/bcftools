3b8809b24a64f0834ec79369610e7db9100464f5
James Bonfield <jkb@sanger.ac.uk>
2026-07-22 16:26:30 +0100

    Add more CI tests for linux containers and BSD/Solaris VMs
    Also modifies sed statement syntax to be more traditional (fix on Solaris)
    
    Signed-off-by: James Bonfield <jkb@sanger.ac.uk>

--- test/trio-dnm3/test.sh
+++ test/trio-dnm3/test.sh
@@ -7,5 +7,5 @@ export LC_ALL=C
 
 for x in *.vcf; do
     "$BCFTOOLS" +trio-dnm3 -p 1X:child,dad,mom $x | "$BCFTOOLS" query -uf$x'\t%CHROM:%POS\t%EXP\t[ %DNM]\t[ %VAF]\t[ %AD]\t%REF,%ALT\t[ %VA]\t[ %SP]';
-done | sort | sed 's,fp,-, ; s,tp,o,'
+done | sort | sed 's/fp/-/;s/tp/o/'
 
